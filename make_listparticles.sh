95966a193a48afdcfabc4611bc196100e1c6f8d1
sonyahanson <sonya.hanson@choderalab.org>
2018-06-04 17:26:36 -0400

    added usage to make_listparticles

--- make_listparticles.sh
+++ make_listparticles.sh
@@ -2,6 +2,7 @@
 
 # This script makes a simple list of particles you can use
 # to reconstruct volumes with reconst_test.bat in Spider.
+# USAGE: ./make_listparticles.sh > listparticles_95k.spi
 
 echo ' ;mat/spi   3-Apr-2018 AT 17:49   listparticles_95k.spi'
 for i in {1..95000}
